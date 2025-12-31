f5270d0258941be730a354b6a42c084443976dbd
Jihyun3478 <leejihyun3478@gmail.com>
2025-12-31 17:41:03 +0900

    fix: 적용된 이벤트가 하나도 없을 경우, -0원이 아닌 0원을 출력하도록 수정

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -54,7 +54,12 @@ public class Application {
 
         System.out.println("\n<총혜택 금액>");
         int totalBenefit = calculator.getTotalBenefit(orders, calendar);
-        System.out.printf("-%,d원%n", totalBenefit);
+        if (totalBenefit == 0) {
+            System.out.printf("%,d원%n", totalBenefit);
+        }
+        if (totalBenefit != 0) {
+            System.out.printf("-%,d원%n", totalBenefit);
+        }
 
         System.out.println("\n<할인 후 예상 결제 금액>");
         int finalAmount = calculator.getFinalAmount(orders, calendar);
